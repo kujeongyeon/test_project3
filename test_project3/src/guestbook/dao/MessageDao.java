01f518efe43f49968d4cdaa5e1c2e8fd463d5af8
cosmo <cosmo@DESKTOP-V56O1AH>
2018-08-04 12:38:25 +0900

    0804_commit

--- test_project3/src/guestbook/dao/MessageDao.java
+++ test_project3/src/guestbook/dao/MessageDao.java
@@ -7,10 +7,80 @@ import guestbook.model.Message;
 import jdbc.JdbcUtil;
 
 public class MessageDao {
-
+	//싱글톤 패턴 적용 (한개 객체만 사용하도록)
+	//유일한 객체를 정적 필드에 저장
 	private static MessageDao messageDao = new MessageDao();
+	//유일한 객체에 접근할 수 있는 정적 메서드 정의 
 	public static MessageDao getInstance() {
 		return messageDao;
 	}
 	
+	//생성자를 외부에 접근하지 못하게함
+	private MessageDao() {}
+	
+	public int insert (Connection conn, Message message) throws SQLException {
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = conn.prepareStatement(
+					"insert into guestbook_message"+
+					"guest_name, password, message) values(?,?,?)");
+			pstmt.setString(1, message.getGuestName());
+			pstmt.setString(2, message.getPassword());
+			pstmt.setString(3, message.getMessage());
+			return pstmt.executeUpdate();
+		} finally {
+			JdbcUtil.close(pstmt);
+		}
+	}
+	
+	public Message select(Connection conn, int messageId) throws SQLException{
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = conn.prepareStatement(
+					"select * from guestbook_message where message_id = ?");
+			pstmt.setInt(1, messageId);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				return makeMessageFromResultSet(rs);
+			} else {
+				return null;
+			}
+		} finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(pstmt);
+		}
+	}
+	private Message makeMessageFromResultSet(ResultSet rs) throws SQLException{
+		Message message = new Message();
+		message.setId(rs.getInt("message_id"));
+		message.setGuestName(rs.getString("guest_name"));
+		message.setPassword(rs.getString("password"));
+		message.setMessage(rs.getString("message"));
+		return message;
+	}
+	
+	public int selectCount(Connection conn) throws SQLException{
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("select count(*) from guestbook_message");
+			rs.next();
+			return rs.getInt(1);
+		}finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(stmt);
+		}
+	}
+	
+	public List<Message> selectList(Connection conn, int firstRow, int endRow) throws SQLException{
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = conn.prepareStatement(
+					)
+		}
+		
+	}
 }
