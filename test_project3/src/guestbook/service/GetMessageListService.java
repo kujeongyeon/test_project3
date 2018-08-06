6cafe85121625507e7dca6b67e40e2c54eac312f
JYeon <cosmos8749@naver.com>
2018-08-07 08:12:17 +0900

    Update GetMessageListService.java
    0807_commit

--- test_project3/src/guestbook/service/GetMessageListService.java
+++ test_project3/src/guestbook/service/GetMessageListService.java
@@ -7,3 +7,41 @@ import guestbook.dao.MessageDao;
 import guestbook.model.Message;
 import jdbc.JdbcUtil;
 import jdbc.connection.ConnectionPorovider;
+
+public class GetMessageListService {
+  private static GetMessageListService instance = new GetMessageListService();
+  
+  public static GetMessageListService getInstance() {
+    return instance;
+  }
+  private GetMessageListService() {
+  }
+  
+  private static final int MESSAGE_COUNT_PER_PAGE = 3;
+  
+  public MessageListView getMessageList(int pageNumber){
+    Connection conn = null;
+    int currentPageNumber = pageNumber;
+    try {
+      conn = ConnectionProvider.getConnection();
+      MessageDao messageDao = MessageDao.getInstance();
+      
+      int messageTotalCount = messageDao.selectCount(conn);
+      
+      List<Message> messageList = null;
+      int firstRow = 0;
+      int endRow = 0;
+      if (messageTotalCount > 0) {
+        firstRow =
+          (pageNumber - 1)*MESSAGE_COUNT_PER_PAGE + 1;
+        endRow = firstRow + MESSAGE_COUNT_PER_PAGE - 1;
+        messageList =
+          messageDao.selectList(conn,firstRow,endRow);
+      }else{
+        currentPageNumber = 0;
+        messageList = Collections.emptyList();
+      }
+      return new MessageListView(messageList, messageTotalCount, currentPageNumber)
+    }
+  }
+}
